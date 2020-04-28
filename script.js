7c7843f6ff5f776dc2af9870911b48bbacea0aed
Justin Abreu <justinabreu92@gmail.com>
2020-04-28 11:55:29 -0500

    got the meter and percentage to display properly

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const strengthMeter = document.getElementById("strength-meter");
 const passwordInput = document.getElementById("password-input");
 const reasonsContainer = document.getElementById("reasons");
+const strengthTotal = document.getElementById("strength");
 
 passwordInput.addEventListener("input", updateStrengthMeter);
 updateStrengthMeter();
@@ -13,11 +14,34 @@ function updateStrengthMeter() {
   weaknesses.forEach((weakness) => {
     if (weakness == null) return;
     strength -= weakness.deduction;
+
     const messageElement = document.createElement("div");
     messageElement.innerText = weakness.message;
     reasonsContainer.appendChild(messageElement);
   });
   strengthMeter.style.setProperty("--strength", strength);
+
+  if (strength < 0) {
+    strength = 0;
+  }
+
+  strengthPercentage(strength);
+}
+
+function strengthPercentage(strength) {
+  strengthTotal.innerHTML = "";
+  const arr = [];
+  const strengthElement = document.createElement("h2");
+  strengthElement.innerText = strength;
+  arr.push(strengthTotal.appendChild(strengthElement));
+
+  const strengthTest = () => {
+    if (arr.length >= 1) {
+      arr.shift();
+    }
+  };
+
+  return strengthTest();
 }
 
 function calculatePasswordStrength(password) {
@@ -26,8 +50,15 @@ function calculatePasswordStrength(password) {
   weaknesses.push(lowercaseWeakness(password));
   weaknesses.push(uppercaseWeakness(password));
   weaknesses.push(numbersWeakness(password));
+  weaknesses.push(specialCharactersWeakness(password));
+  weaknesses.push(repeatCharactersWeakness(password));
+
   return weaknesses;
 }
+
+// function scoreCheck(password) {
+//     if ()
+// }
 
 function lengthWeakness(password) {
   const length = password.length;
@@ -55,6 +86,18 @@ function lowercaseWeakness(password) {
   return characterTypeWeakeness(password, /[a-z]/g, "lowercase characters");
 }
 
+function numbersWeakness(password) {
+  return characterTypeWeakeness(password, /[0-9]/g, "numbers");
+}
+
+function specialCharactersWeakness(password) {
+  return characterTypeWeakeness(
+    password,
+    /[^0-9a-zA-Z\s]/g,
+    "special characters"
+  );
+}
+
 function characterTypeWeakeness(password, regex, type) {
   const matches = password.match(regex) || [];
 
@@ -73,6 +116,13 @@ function characterTypeWeakeness(password, regex, type) {
   }
 }
 
-function numbersWeakness(password) {
-  return characterTypeWeakeness(password, /[0-9]/g, "numbers");
+function repeatCharactersWeakness(password) {
+  const matches = password.match(/(.)\1/g) || [];
+
+  if (matches.length > 0) {
+    return {
+      message: "Your password has repeat characters",
+      deduction: matches.length * 10,
+    };
+  }
 }
